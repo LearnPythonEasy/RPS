f6b52324fcd4c2ae91c8fe172d93db8371ec330c
ACA30 <ACA30@users.noreply.github.com>
2017-10-18 08:29:24 -0400

    Update main.py
    - Added the option to play again.
    - Added credits
    - Added license

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 #!/bin/python3
 
 from random import randint
-  
-player = input('rock (r), paper (p) or scissors (s)?')
 
-if(player == 'r'):
-  print('O', end=' ')
-  
-elif(player == 'p'):
-  print('___', end=' ')
-  
-elif(player == 's'):
-  print('>8', end=' ')
-  
-else:
-  print('??')
-  
-print('vs', end=' ')
+#Rock Paper Scissors Game
+#Created by Nate (http://github.com/ACA30)
 
-chosen = randint(1,3)
+while True:
+    player = input('rock (r), paper (p) or scissors (s): ')
 
-if(chosen == 1):
-  computer = 'r'
-  print('O')
+    if(player == 'r'):
+      print('O', end=' ')
   
-elif(chosen == 2):
-  computer = 'p'
-  print('___')
+    elif(player == 'p'):
+      print('___', end=' ')
   
-else:
-  computer = 's'
-  print('>8')
+    elif(player == 's'):
+      print('8<', end=' ')
+  
+    else:
+      print('??')
+  
+    print('vs', end=' ')
 
-if(player == computer):
-  print('DRAW!')
-  
-elif(player == 'r' and computer == 's'):
-  print('Player wins!')
-  
-elif(player == 'r' and computer == 'p'):
-  print('Computer wins!')
-  
-elif(player == 'p' and computer == 'r'):
-  print('Player wins!')
-  
-elif(player == 'p' and computer == 's'):
-  print('Computer wins!')
+    chosen = randint(1,3)
 
-elif(player == 's' and computer == 'p'):
-  print('Player wins!')
+    if(chosen == 1):
+      computer = 'r'
+      print('O')
   
-elif(player == "s" and computer == 'r'):
-  print('Computer wins!')
+    elif(chosen == 2):
+      computer = 'p'
+      print('___')
+  
+    else:
+      computer = 's'
+      print('>8')
 
-else:
-  print('Huh?')
+    if(player == computer):
+      print('DRAW!')
   
+    elif(player == 'r' and computer == 's'):
+      print('Player wins!')
   
+    elif(player == 'r' and computer == 'p'):
+      print('Computer wins!')
+  
+    elif(player == 'p' and computer == 'r'):
+      print('Player wins!')
+  
+    elif(player == 'p' and computer == 's'):
+      print('Computer wins!')
+
+    elif(player == 's' and computer == 'p'):
+      print('Player wins!')
+  
+    elif(player == "s" and computer == 'r'):
+      print('Computer wins!')
+
+    else:
+      print('Huh? Something broke! Please restart the program!')
+
+    response = input('Would you like to play again? y or n: ')
+    if response == 'n':
+        print('')
+        print('')
+        print('Thank you for playing the Rock Paper Scissors Game!')
+        print('Created by Nate - http://github.com/ACA30')
+        print('License can be found at: https://github.com/LearnPythonEasy/RPS/blob/master/LICENSE')
+        print('')
+        print('')
+        break
